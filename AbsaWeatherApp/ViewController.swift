91a6c0750406795ec5eb0107688c702025b4a43d
Sibusiso Sibisi <sbudasibisi@icloud.com>
2020-08-05 13:11:23 +0200

    Initial Commit

---
+++ AbsaWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AbsaWeatherApp
+//
+//  Created by Sibusiso on 2020/08/05.
+//  Copyright © 2020 Sibusiso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
